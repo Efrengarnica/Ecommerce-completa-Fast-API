25b38bd2b5b87bf242589c0d142e1e65ea4bec55
Efren Garnica Palacios <efrengarnicapalacios@gmail.com>
2026-03-27 23:03:05 -0600

    Se crea conexión entre el archivo que maneja las excepciones con mi app.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, status, Request
 from fastapi.responses import JSONResponse
 
+#Importo mi funcion que implementa el manejador de excepciones.
+from handlers import register_exception_handlers
 
 #rutas
 from api.ejemplo_router import router as ejemplo_router
@@ -43,9 +45,17 @@ Si la app tiene un exception handler entonces la app no lanza la excepcion
 la maneja y lanza solo el mensaje.
  """
 
-@app.exception_handler(status.HTTP_404_NOT_FOUND)
+""" @app.exception_handler(status.HTTP_404_NOT_FOUND)
 async def custom_404_handler(request: Request, exc: Exception):
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND,
         content={"estado":"error", "mensaje":"Ruta no encontrada"}
-    )
+    ) """
+#Arriba está una manera de manejar excepciones pero si queremos crecer esta es la manera
+#Aqui se implementa el último paso para conectar la app con el manejador.
+""" No es necesario manejar todos los errores la idea es que se manejen los reconocidos para ti
+asi tu front sabra que hacer y sabra que presentar.
+Otra razon es que no se detiene la app pero hay ocasiones que si no se maneja se da más info de la necesaria y alguien que sepa
+puede ocupar esa ventana para entrar por medio de lo que diga el error.
+ """
+register_exception_handlers(app)
