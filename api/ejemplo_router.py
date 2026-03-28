fc0a6ca2e353c6e85ff4aa7b1511d097b4038412
Efren Garnica Palacios <efrengarnicapalacios@gmail.com>
2026-03-27 19:58:10 -0600

    Se crea un ejemplo de router y se agrega a la app.

---
+++ api/ejemplo_router.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+
+
+#La idea es que ahora aquí se definan todos los endpoints relacionado con ejemplo.
+#Primero se define el router.
+#Para que mi router este asociado a mi app debo de ir a mi app a decirle que este router estará asociado con ella.
+router = APIRouter(prefix="/ejemplo", tags=["Ejemplo"])
+
+
+@router.get("/")
+async def index():
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"De donde me hablas": "De ejemplo"}
+    )
+
+
+#Cuando haces esto fastapi detecta la variable en la ruta y lo puede rescatar.
+@router.get("/{id}")
+async def show(id:int):
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"estado":"ok", "mensaje":f"Método GET | id={id}"}
+    )
+
+
+@router.post("/")
+async def create():
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={"estado":"ok", "mensaje": "Método POST"}
+    )
+
+
+@router.put("/{id}")
+async def update(id:int):
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"estado":"ok", "mensaje": f"Método PUT | id={id}"}
+    )
+
+
+@router.delete("/{id}")
+async def destroy(id:int):
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"estado":"ok", "mensaje": f"Método DELETE | id={id}"}
+    )
