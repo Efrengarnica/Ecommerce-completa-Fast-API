a7d72bdebfde2d3b7c907eb5c40270c7dbe3c79f
Efren Garnica Palacios <efrengarnicapalacios@gmail.com>
2026-03-31 23:33:58 -0600

    Se modifica la manera en que se convive con s3 de modo que se pueda trabajar de manera async.

--- infrastructure/s3_storage.py
+++ infrastructure/s3_storage.py
@@ -1,21 +1,16 @@
 from fastapi import UploadFile
 from config.aws import s3_client, S3_BUCKET_NAME
 import uuid
+from exceptions.s3_exceptions import InvalidImageTypeException, S3UploadException, S3ImageDeleteError, S3ImageNotFound
 
-class InvalidImageTypeException(Exception):
-    pass
-
-class S3UploadException(Exception):
-    pass
-
+# Subir archivos a S3.
 def upload_product_image(file: UploadFile) -> str:
     if file.content_type == "image/png":
         extension = "png"
     elif file.content_type in ("image/jpeg", "image/jpg"):
         extension = "jpg"
     else:
-        #La tengo que crear aparte y decidir que mensaje va a mostrar.
-        raise InvalidImageTypeException("Formato de imagen no permitido")
+        raise InvalidImageTypeException(file.content_type)
 
     nombre = f"{uuid.uuid4()}.{extension}"
 
@@ -27,7 +22,21 @@ def upload_product_image(file: UploadFile) -> str:
             ExtraArgs={"ContentType": file.content_type},
         )
     except Exception as e:
-        #La tengo que crear de igual manera y saber que hacer.
-        raise S3UploadException(f"No se pudo subir la imagen: {str(e)}")
+        raise S3UploadException(file.filename)
 
     return f"http://localhost:8000/{S3_BUCKET_NAME}/archivos/{nombre}"
+
+
+# Borrar archivos de S3.
+def delete_image_from_s3(link:str) -> None:
+    file_name = ""
+    if "/archivos/" in link:
+        file_name = link.split("/archivos/")[-1]
+    try:    
+        s3_client.head_object(Bucket=S3_BUCKET_NAME, Key=f"archivos/{file_name}")
+    except s3_client.exceptions.ClientError as e:
+        raise S3ImageNotFound(file_name)
+    try:
+        s3_client.delete_object(Bucket=S3_BUCKET_NAME, Key=f"archivos/{file_name}")
+    except Exception as e:
+        raise S3ImageDeleteError(file_name)
