e381a7e5298a916cc3a4c2d3ff32ece829e0d044
Efren Garnica Palacios <efrengarnicapalacios@gmail.com>
2026-04-02 00:43:48 -0600

    Se crea validation exception handler para que se muestre un mejor mensaje cuando se despliegue un Request Validation Error.

--- handlers/generic.py
+++ handlers/generic.py
@@ -1,10 +1,32 @@
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException
+from fastapi.exceptions import RequestValidationError
 
-#Recuerda que es async ya que esta función puede ser llamada muchas veces.
+
+# Recuerda que es async ya que esta función puede ser llamada muchas veces.
 async def custom_404_handler(request: Request, exc: HTTPException):
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND,
         content={"status": "error", "message": str(exc.detail)}
+    )
+
+
+# Me sirve para manejar los RequestValidationError.
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errores = []
+    for err in exc.errors():
+        campo = ".".join(str(x) for x in err["loc"] if x != "body")
+        errores.append({
+            "campo": campo,
+            "mensaje": err["msg"]
+        })
+
+    return JSONResponse(
+        status_code=422,
+        content={
+            "ok": False,
+            "mensaje": "Hay errores en los datos enviados.",
+            "errores": errores
+        }
     )
