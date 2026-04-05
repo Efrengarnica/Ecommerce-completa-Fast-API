0855882abbdf0eb0e14f492d49b7c3b38cc45440
Efren Garnica Palacios <efrengarnicapalacios@gmail.com>
2026-04-05 17:45:42 -0600

    Se crea función que valida el token que viene en una cookie.

--- dependencies/security.py
+++ dependencies/security.py
@@ -1,8 +1,8 @@
-from fastapi import Depends, HTTPException, status, Header
+from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from dotenv import load_dotenv
-load_dotenv()
+from jose import jwt, JWTError, ExpiredSignatureError
 import os
+
 
 
 admin_token = os.getenv("ADMIN_TOKEN")
@@ -22,3 +22,22 @@ async def validate_auth(credentials: HTTPAuthorizationCredentials = Depends(secu
             headers={"WWW-Authenticate": "Bearer"},
         )
     return token
+
+
+# Me ayuda a poder obtener el id del usuario y comprobar que el token es válido y que la fecha de expiración sea válida
+async def get_current_user_id(request: Request):
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(status_code=401, detail="No hay sesión activa")
+    
+    try:
+        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=[os.getenv("ALGORITHM")])
+        user_id = payload.get("sub")
+        return user_id
+
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="La sesión ha expirado")
+        
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Error de autenticación")
